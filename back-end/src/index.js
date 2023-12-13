6d153152267c83981c845044d76c93cfd939ff82
andiseo <andiseyo50@gmail.com>
2023-12-13 15:10:19 +0700

    update index.js

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -4,10 +4,10 @@ const UserRoute = require('./routes/users.js')
 const middlewareLogRequest = require('./middleware/logs.js')
 require('dotenv').config()
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
